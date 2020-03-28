c96a5cb93cc0b3be6becade2a2b7f64239eb725a
wert2all <wert2all@gmail.com>
2020-03-28 13:31:40 +0200

    Try to fetch auth data

--- src/modules/auth/AuthCheck.js
+++ src/modules/auth/AuthCheck.js
@@ -1,3 +1,5 @@
+const {OAuth2Client} = require('google-auth-library');
+
 /**
  * @class AuthCheck
  */
@@ -7,7 +9,8 @@ export default class AuthCheck {
      * @param {string} apiKey
      */
     constructor(apiKey) {
-
+        this.apiKey = apiKey;
+        this.client = new OAuth2Client(apiKey, '', '');
     }
 
     /**
@@ -15,6 +18,26 @@ export default class AuthCheck {
      * @param {AuthParams} params
      */
     check(params) {
+        /**
+         *
+         * @param {AuthCheck} self
+         * @param {AuthParams} params
+         * @return {Promise<void>}
+         */
+        async function verify(self, params) {
+            const ticket = await self.client.verifyIdToken({
+                idToken: params.token,
+                audience: self.apiKey,  // Specify the CLIENT_ID of the app that accesses the backend
+                // Or, if multiple clients access the backend:
+                //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+            });
+            const payload = ticket.getPayload();
+            const userid = payload['sub'];
+            // If request specified a G Suite domain:
+            //const domain = payload['hd'];
+        }
 
+        console.log(verify(this, params));
+        // verify(this, params).catch(console.error);
     }
 }

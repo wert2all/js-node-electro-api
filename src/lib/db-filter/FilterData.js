ecb30bb75be62d148b759e4d4871670b72c13e8c
wert2all <wert2all@gmail.com>
2020-04-21 14:46:31 +0300

    Implement SQLiteConnection._buildSelect

---
+++ src/lib/db-filter/FilterData.js
@@ -0,0 +1,10 @@
+/**
+ * @class FilterData
+ */
+export default class FilterData {
+    constructor(field, sign, value) {
+        this.field = field;
+        this.sign = sign;
+        this.value = value;
+    }
+}

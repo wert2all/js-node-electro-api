62d97c4c34c3ce7d2e70c021c1db2c3ec1b68588
wert2all <wert2all@gmail.com>
2020-04-21 14:31:39 +0300

    Implement SQLiteConnection.select

--- src/lib/db-connection/ConnectionInterface.js
+++ src/lib/db-connection/ConnectionInterface.js
@@ -2,15 +2,15 @@ import ImplementationError from '../implementation-error/ImplementationError';
 
 /**
  * @interface
+ * @abstract
  * @class ConnectionInterface
  */
 export default class ConnectionInterface {
     /**
-     *
+     * @abstract
      * @param {DefinitionTableInterface} definition
      * @param {FilterInterface} filter
      * @return {Promise<Array>}
-     * @abstract
      */
     async select(definition, filter) {
         throw new ImplementationError(this, 'select');
--- src/lib/db-connection/adapter/SQLiteConnection.js
+++ src/lib/db-connection/adapter/SQLiteConnection.js
@@ -1,4 +1,5 @@
 import ConnectionInterface from '../ConnectionInterface';
+import ImplementationError from '../../implementation-error/ImplementationError';
 
 /**
  * @class SQLiteConnection
@@ -14,5 +15,46 @@ export default class SQLiteConnection extends ConnectionInterface {
         super();
     }
 
+    /**
+     *
+     * @param {DefinitionTableInterface} definition
+     * @param {FilterInterface} filter
+     * @return {Promise<Array>}
+     */
+    async select(definition, filter) {
+        const sql = this._buildSelect(definition, filter);
+        return this._query(sql);
+    }
 
+    /**
+     *
+     * @param {DefinitionTableInterface} definition
+     * @param {FilterInterface} filter
+     * @return {string}
+     * @private
+     */
+    _buildSelect(definition, filter) {
+        throw  new ImplementationError(this, '_buildSelect');
+    }
+
+    /**
+     *
+     * @param {string} sql
+     * @return {Promise<Array>}
+     * @private
+     */
+    async _query(sql) {
+        const connection = await this._connect();
+
+        return Promise.reject(new ImplementationError(this, '_query'));
+    }
+
+    /**
+     *
+     * @return {Promise<>}
+     * @private
+     */
+    async _connect() {
+        return Promise.reject(new ImplementationError(this, '_connect'));
+    }
 }

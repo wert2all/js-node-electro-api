1066dc96e586ac554c2276216c08e33d552272a6
wert2all <wert2all@gmail.com>
2020-04-21 12:47:32 +0300

    Create DataValue object

---
+++ src/lib/data-value/DataValue.js
@@ -0,0 +1,35 @@
+import DataValueInterface from './DataValueInterface';
+
+/**
+ * @class DataValue
+ * @type DataValueInterface
+ * @extends DataValueInterface
+ */
+export default class DataValue extends DataValueInterface {
+    constructor() {
+        super();
+        /**
+         *
+         * @type {Object<string, string>}
+         * @private
+         */
+        this._data = {};
+    }
+
+    /**
+     * @param  {string} key
+     * @return {string||null}
+     */
+    getData(key) {
+        return this._data[key];
+    }
+
+    /**
+     * @abstract
+     * @param {string} key
+     * @param {string} value
+     */
+    setData(key, value) {
+        this._data[key] = value;
+    }
+}

d1e256e6862ccac6b72449d0d8edd204fd988ffd
wert2all <wert2all@gmail.com>
2020-04-21 12:18:52 +0300

    Create FilterFactory

--- src/db/repository/FilesRepository.js
+++ src/db/repository/FilesRepository.js
@@ -3,8 +3,23 @@
  *
  */
 import RepositoryAbstract from '../../lib/db-repository/ReposytoryAbstract';
+import FilterFactory from '../../lib/db-filter/FilterFactory';
 
 export default class FilesRepository extends RepositoryAbstract {
+
+    /**
+     *
+     * @param {ConnectionInterface} connection
+     */
+    constructor(connection) {
+        super(connection);
+        /**
+         *
+         * @type {FilterFactoryInterface}
+         * @private
+         */
+        this._filterFactoryObject = new FilterFactory();
+    }
 
     /**
      *
@@ -14,4 +29,13 @@ export default class FilesRepository extends RepositoryAbstract {
     async fetchData(userFilesEntity) {
         return super.fetchData(userFilesEntity);
     }
+
+    /**
+     * @protected
+     * @return FilterFactoryInterface
+     */
+    _filterFactory() {
+        return this._filterFactoryObject;
+    }
+
 }

8fe5ebea022ab9165f5d54ca1fc60a9de1f78736
wert2all <wert2all@gmail.com>
2020-04-21 12:21:37 +0300

    Create Filter

---
+++ src/lib/db-filter/Filter.js
@@ -0,0 +1,10 @@
+import FilterInterface from './FilterInterface';
+
+/**
+ * @class Filter
+ * @type FilterInterface
+ * @extends FilterInterface
+ */
+export default class Filter extends FilterInterface {
+
+}

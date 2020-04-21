e12b9b02b344349c9ff7abdaaec6286bfc5945c9
wert2all <wert2all@gmail.com>
2020-04-21 14:08:49 +0300

    Implement Filter.addFilterData

--- src/lib/db-filter/Filter.js
+++ src/lib/db-filter/Filter.js
@@ -6,5 +6,21 @@ import FilterInterface from './FilterInterface';
  * @extends FilterInterface
  */
 export default class Filter extends FilterInterface {
+    constructor() {
+        super();
+        /**
+         *
+         * @type {Object<string, string>}
+         * @private
+         */
+        this._filters = {};
+    }
 
+    /**
+     * @param {DefinitionColumnInterface} column
+     * @param {string} value
+     */
+    addColumn(column, value) {
+        this._filters[column.getColumnName()] = value;
+    }
 }

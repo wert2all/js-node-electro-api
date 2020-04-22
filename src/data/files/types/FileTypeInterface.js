e69184c25a989bb99b07d30347fd864bd0196446
wert2all <wert2all@gmail.com>
2020-04-22 17:14:28 +0300

    Implement ImageProcess.checkType

--- src/data/files/types/FileTypeInterface.js
+++ src/data/files/types/FileTypeInterface.js
@@ -1,8 +1,17 @@
+import ImplementationError from '../../../lib/implementation-error/ImplementationError';
+
 /**
  * @interface
  * @abstract
  * @class FileTypeInterface
  */
 export default class FileTypeInterface {
+    /**
+     * @abstract
+     * @return {string}
+     */
+    getType() {
+        throw new ImplementationError(this, 'getType');
+    }
 
 }

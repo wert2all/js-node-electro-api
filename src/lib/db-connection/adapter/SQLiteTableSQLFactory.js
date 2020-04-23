b9350c1f032ccccd464d5c1c1710f0393bfc3185
wert2all <wert2all@gmail.com>
2020-04-23 14:30:38 +0300

    Fix create table with default

--- src/lib/db-connection/adapter/SQLiteTableSQLFactory.js
+++ src/lib/db-connection/adapter/SQLiteTableSQLFactory.js
@@ -1,5 +1,6 @@
 // eslint-disable-next-line max-len
 import TableSQLFactoryInterface from '../../db-definition/DefinitionTableSQLFactoryInterface';
+import DefinitionColumn from '../../db-definition/DefinitionColumn';
 
 /**
  * @class SQLiteTableSQLFactory
@@ -23,14 +24,38 @@ export default class SQLiteTableSQLFactory extends TableSQLFactoryInterface {
      * @return string
      */
     _columnsSQL(definition) {
+        console.log();
         return definition.getColumns().map(column => {
             let columnSQL = ' ' + column.getColumnName() + ' ' + column.getType() + ' ';
             columnSQL += (!column.isNull()) ? ' not null ' : ' ';
-            columnSQL += (column.getDefault() !== false)
-                ? ' default ' + column.getDefault()
-                : ' ';
+            columnSQL += this._createDefault(column);
             columnSQL += (column.isPrimary()) ? ' primary key ' : ' ';
             return columnSQL;
         }).join(',\n');
+    }
+
+    /**
+     *
+     * @param {DefinitionColumnInterface} column
+     * @return {string}
+     * @private
+     */
+    _createDefault(column) {
+        return (column.getDefault() !== false)
+            ? (' default ' + this._wrapValue(column, column.getDefault()))
+            : ' ';
+    }
+
+    /**
+     *
+     * @param {DefinitionColumnInterface} column
+     * @param {*} value
+     * @return {string}
+     * @private
+     */
+    _wrapValue(column, value) {
+        return (column.getType() === DefinitionColumn.COLUMN_TYPE_VARCHAR)
+            ? '"' + value + '"'
+            : value;
     }
 }

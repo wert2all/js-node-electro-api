3a9bcf03ed61b7d6547fbee8980804de214f9a9a
wert2all <wert2all@gmail.com>
2020-05-01 14:57:47 +0300

    Replace all

---
+++ src/lib/utils/StringExt.js
@@ -0,0 +1,27 @@
+/**
+ * @class StringExt
+ */
+export default class StringExt {
+    /**
+     *
+     * @param {string} value
+     */
+    constructor(value) {
+        /**
+         *
+         * @type {string}
+         * @private
+         */
+        this._string = value;
+    }
+
+    /**
+     *
+     * @param {string} search
+     * @param {string} replacement
+     * @return {string}
+     */
+    replaceAll(search, replacement) {
+        return this._string.replace(new RegExp(search, 'g'), replacement);
+    }
+}

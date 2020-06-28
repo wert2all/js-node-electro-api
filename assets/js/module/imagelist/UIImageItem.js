ded9adc21e8b9304d8aecdc71342e32f4befe69a
wert2all <wert2all@gmail.com>
2020-06-28 15:43:28 +0300

    ui: images: creating image items

--- assets/js/module/imagelist/UIImageItem.js
+++ assets/js/module/imagelist/UIImageItem.js
@@ -6,5 +6,41 @@ import UIElementInterface from '../../ui/interfaces/element/UIElementInterface';
  * @type UIElementInterface
  */
 export default class UIImageItem extends UIElementInterface {
+    /**
+     *
+     * @param {Node} itemNode
+     */
+    constructor(itemNode) {
+        super();
+        /**
+         *
+         * @type {Node}
+         * @private
+         */
+        this._node = itemNode;
+    }
 
+    clean() {
+    }
+
+    clone() {
+        return new UIImageItem(this._node.cloneNode(true));
+    }
+
+    getNode() {
+        return this._node;
+    }
+
+    init() {
+    }
+
+    /**
+     *
+     * @param {ImageData} imageData
+     * @return {UIImageItem}
+     */
+    // eslint-disable-next-line no-unused-vars
+    create(imageData) {
+        return this.clone();
+    }
 }

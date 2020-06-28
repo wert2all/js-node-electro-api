65ccc599a7af195e0df62549fe9ba0529e9aac5f
wert2all <wert2all@gmail.com>
2020-06-28 13:49:52 +0300

    ui: images: creating image items

---
+++ assets/js/module/imagelist/UIImageItem.js
@@ -0,0 +1,10 @@
+import UIElementInterface from '../../ui/interfaces/element/UIElementInterface';
+
+/**
+ * @class UIImageItem
+ * @extends UIElementInterface
+ * @type UIElementInterface
+ */
+export default class UIImageItem extends UIElementInterface {
+
+}

960e66cc98962620f42642cfd49c6398d1880614
wert2all <wert2all@gmail.com>
2020-07-25 00:49:13 +0300

    api: delete image: getting request data

---
+++ src/modules/images/data/ImagesDeleteDataClass.js
@@ -0,0 +1,94 @@
+import StringExt from '../../../lib/utils/StringExt';
+import ImagesGetNoToken from '../error/ImagesGetNoToken';
+
+export default class ImagesDeleteDataClass {
+    /**
+     *
+     * @param {string} token
+     * @param {number} imageId
+     */
+    constructor(token, imageId) {
+        /**
+         *
+         * @type {string}
+         * @private
+         */
+        this._token = token;
+        /**
+         *
+         * @type {GoogleAccount|null}
+         * @private
+         */
+        this._account = null;
+        /**
+         *
+         * @type {number}
+         * @private
+         */
+        this._imageId = imageId;
+    }
+
+    /**
+     *
+     * @param request
+     * @return {ImagesDeleteDataClass}
+     */
+    static factory(request) {
+        let authToken = null;
+        let returnRequest = null;
+        if (request.body.token) {
+            authToken = Buffer.from(
+                new StringExt(request.body.token)
+                    .replaceAll('"', ''),
+                'base64'
+            ).toString();
+        }
+        const id = parseInt(request.body.image, 10);
+        if (authToken !== null) {
+            returnRequest = new ImagesDeleteDataClass(authToken, id);
+        } else {
+            throw new ImagesGetNoToken();
+        }
+
+        return returnRequest;
+    }
+
+    /**
+     *
+     * @return {number}
+     */
+    getImageId() {
+        return this._imageId;
+    }
+
+    /**
+     *
+     * @return {GoogleAccount|null}
+     */
+    getAccount() {
+        return this._account;
+    }
+
+    /**
+     *
+     * @param {GoogleAccount} account
+     * @return {ImagesDeleteDataClass}
+     */
+    setGoogleAccount(account) {
+        /**
+         *
+         * @type {string}
+         * @private
+         */
+        this._account = account;
+        return this;
+    }
+
+    /**
+     *
+     * @return {string}
+     */
+    getToken() {
+        return this._token;
+    }
+}

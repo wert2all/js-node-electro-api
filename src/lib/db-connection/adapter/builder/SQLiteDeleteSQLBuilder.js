f23f9187b4f8340c6b15beedf45e763b98f61ccf
wert2all <wert2all@gmail.com>
2020-07-26 12:45:29 +0300

    lib: add delete method to ConnectionInterface

---
+++ src/lib/db-connection/adapter/builder/SQLiteDeleteSQLBuilder.js
@@ -0,0 +1,33 @@
+import DefinitionSQLBuilderInterface from '../../../db-definition/builder/DefinitionSQLBuilderInterface';
+import ErrorConnection from '../../error/ErrorConnection';
+
+/**
+ * @class SQLiteDeleteSQLBuilder
+ * @type DefinitionSQLBuilderInterface
+ * @extends DefinitionSQLBuilderInterface
+ */
+export default class SQLiteDeleteSQLBuilder extends DefinitionSQLBuilderInterface {
+    /**
+     *
+     * @param {DefinitionTableInterface} definition
+     * @param {Object<string, string>} data
+     * @return string
+     */
+    buildSQL(definition, data) {
+        const primaryValue = (
+            data.hasOwnProperty(
+                definition.getPrimaryColumn().getColumnName()
+            )
+        )
+            ? data[definition.getPrimaryColumn().getColumnName()]
+            : false;
+        if (primaryValue !== false) {
+            let sql = 'delete from ' + definition.getTableName();
+            sql += ' where ' + definition.getPrimaryColumn().getColumnName() +
+                ' = :' + definition.getPrimaryColumn().getColumnName();
+            return sql;
+        } else {
+            throw new ErrorConnection();
+        }
+    }
+}

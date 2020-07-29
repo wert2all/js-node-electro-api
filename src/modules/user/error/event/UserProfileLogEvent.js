7fcb2eca1cc260d90fd6f02707d9a00db066d95c
wert2all <wert2all@gmail.com>
2020-07-29 11:06:11 +0300

    perf(api): create LogEvent class for user profile errors
    МЕТА ДАННЫЕ: #301

---
+++ src/modules/user/error/event/UserProfileLogEvent.js
@@ -0,0 +1,48 @@
+import LogEventInterface from '../../../../lib/logger/LogEventInterface';
+
+/**
+ * @class UserProfileLogEvent
+ * @type LogEventInterface
+ * @extends LogEventInterface
+ */
+export default class UserProfileLogEvent extends LogEventInterface {
+    static TAG = 'user_profile';
+
+    /**
+     *
+     * @param {string} message
+     */
+    constructor(message) {
+        super();
+        /**
+         *
+         * @type {string}
+         * @private
+         */
+        this._message = message;
+    }
+
+    /**
+     *
+     * @return {string}
+     */
+    getMessage() {
+        return this._message;
+    }
+
+    /**
+     *
+     * @return {string}
+     */
+    getTag() {
+        return UserProfileLogEvent.TAG;
+    }
+
+    /**
+     *
+     * @return {Date}
+     */
+    getTime() {
+        return new Date();
+    }
+}

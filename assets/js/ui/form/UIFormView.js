34db0365ab9256ca2b6b2a5d80d85445b1c45dcc
wert2all <wert2all@gmail.com>
2020-07-29 23:29:24 +0300

    feat(ui): add loader to user profile modal
    МЕТА ДАННЫЕ: #306

---
+++ assets/js/ui/form/UIFormView.js
@@ -0,0 +1,93 @@
+import UIFormViewInterface from './UIFormViewInterface';
+
+/**
+ * @class UIFormView
+ * @extends UIFormViewInterface
+ * @type UIFormViewInterface
+ */
+export default class UIFormView extends UIFormViewInterface {
+    /**
+     *
+     * @param {DomForm} formDom
+     * @param {HTMLFormElement} formNode
+     * @param {UIElementInterface} loader
+     */
+    constructor(formDom, formNode, loader) {
+        super();
+        /**
+         *
+         * @type {DomForm}
+         * @private
+         */
+        this._form = formDom;
+        /**
+         *
+         * @type {HTMLFormElement}
+         * @private
+         */
+        this._formNode = formNode;
+        /**
+         *
+         * @type {UIElementInterface}
+         * @private
+         */
+        this._loader = loader;
+        /**
+         *
+         * @type {null| UIElementInterface}
+         * @private
+         */
+        this._showedLoader = null;
+    }
+
+    /**
+     *
+     * @param {function} submitFunction
+     */
+    init(submitFunction) {
+        this._form.init(submitFunction);
+    }
+
+    /**
+     *
+     * @param {string} elementName
+     * @param {string} value
+     * @return {UIFormViewInterface}
+     */
+    setElement(elementName, value) {
+        this._form.setElement(elementName, value);
+        return this;
+    }
+
+    /**
+     *
+     * @return {Promise<boolean>}
+     */
+    submit() {
+        return Promise.resolve(false);
+    }
+
+    /**
+     *
+     * @return {UIFormViewInterface}
+     */
+    hideLoader() {
+        this._formNode.style.visibility = 'visible';
+        if (this._showedLoader != null) {
+            this._formNode.parentNode.removeChild(this._showedLoader);
+        }
+        this._showedLoader = null;
+        return this;
+    }
+
+    /**
+     *
+     * @return {UIFormViewInterface}
+     */
+    showLoader() {
+        this._formNode.style.visibility = 'hidden';
+        this._showedLoader = this._loader.clone().getNode();
+        this._formNode.parentNode.append(this._showedLoader);
+        return this;
+    }
+}
---
+++ assets/js/ui/form/UIFormViewInterface.js
@@ -0,0 +1,22 @@
+import DomFormInterface from '../../dom/form/DomFormInterface';
+
+/**
+ * @interface
+ * @abstract
+ * @class UIFormViewInterface
+ */
+export default class UIFormViewInterface extends DomFormInterface {
+    /**
+     * @return UIFormViewInterface
+     */
+    showLoader() {
+
+    }
+
+    /**
+     * @return UIFormViewInterface
+     */
+    hideLoader() {
+
+    }
+}

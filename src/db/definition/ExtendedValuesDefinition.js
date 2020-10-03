a4c42f285b2bc7425b3e09243989b26b302c8e71
wert2all <wert2all@gmail.com>
2020-10-03 22:17:20 +0300

    feat(api): create definition of extension values
    МЕТА ДАННЫЕ: #373

---
+++ src/db/definition/ExtendedValuesDefinition.js
@@ -0,0 +1,99 @@
+import DefinitionTableInterface from '../../lib/db-definition/DefinitionTableInterface';
+import DefinitionColumn from '../../lib/db-definition/DefinitionColumn';
+
+/**
+ * @class ExtendedValuesDefinition
+ * @type DefinitionTableInterface
+ * @extends DefinitionTableInterface
+ */
+export default class ExtendedValuesDefinition extends DefinitionTableInterface {
+    static TABLE_NAME = 'ext_values';
+    static COLUMN_ID = 'id';
+    static COLUMN_ENTITY_ID = 'entity_id';
+    static COLUMN_ENTITY_TYPE = 'entity_type';
+    static COLUMN_VALUE_NAME = 'entity_value_name';
+    static COLUMN_VALUE_VALUE = 'entity_value_value';
+
+    constructor() {
+        super();
+
+        this._primaryColumn = new DefinitionColumn(
+            ExtendedValuesDefinition.COLUMN_ID,
+            DefinitionColumn.COLUMN_TYPE_INTEGER,
+            false,
+            false,
+            true
+        );
+
+        /**
+         *
+         * @type {DefinitionColumnInterface[]}
+         * @private
+         */
+        this._columns = [
+            this._primaryColumn,
+            new DefinitionColumn(
+                ExtendedValuesDefinition.COLUMN_ENTITY_ID,
+                DefinitionColumn.COLUMN_TYPE_INTEGER,
+                false,
+                false,
+                false
+            ),
+            new DefinitionColumn(
+                ExtendedValuesDefinition.COLUMN_ENTITY_TYPE,
+                DefinitionColumn.COLUMN_TYPE_VARCHAR,
+                false,
+                false,
+                false
+            ),
+            new DefinitionColumn(
+                ExtendedValuesDefinition.COLUMN_VALUE_NAME,
+                DefinitionColumn.COLUMN_TYPE_VARCHAR,
+                false,
+                false,
+                false
+            ),
+            new DefinitionColumn(
+                ExtendedValuesDefinition.COLUMN_VALUE_VALUE,
+                DefinitionColumn.COLUMN_TYPE_VARCHAR,
+                true,
+                false,
+                false
+            ),
+        ];
+    }
+
+    /**
+     *
+     * @return {string}
+     */
+    getTableName() {
+        return ExtendedValuesDefinition.TABLE_NAME;
+    }
+
+    /**
+     *
+     * @return {DefinitionColumn}
+     */
+    getPrimaryColumn() {
+        return this._primaryColumn;
+    }
+
+    /**
+     * @param {string} key
+     * @return {boolean}
+     */
+    isColumn(key) {
+        return this.getColumns()
+            .map(definition => definition.getColumnName())
+            .includes(key);
+    }
+
+    /**
+     *
+     * @return {DefinitionColumnInterface[]}
+     */
+    getColumns() {
+        return this._columns;
+    }
+}

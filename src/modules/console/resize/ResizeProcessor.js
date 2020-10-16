73d5185eff8f22f591acc24ae2b8aa51ed281535
wert2all <wert2all@gmail.com>
2020-10-16 11:06:33 +0300

    feat(infra): create resize config
    МЕТА ДАННЫЕ: #416

--- src/modules/console/resize/ResizeProcessor.js
+++ src/modules/console/resize/ResizeProcessor.js
@@ -7,11 +7,30 @@ import ProcessorInterface from "../../../lib/console/gulp/processor/ProcessorInt
  */
 export default class ResizeProcessor extends ProcessorInterface {
     /**
+     *
+     * @param {ResizeSizesHolder} sizes
+     */
+    constructor(sizes) {
+        super();
+        /**
+         *
+         * @type {ResizeSizesHolder}
+         * @private
+         */
+        this._sizes = sizes;
+    }
+
+    /**
      * @param {UserFilesEntity} entity
      * @param {ImageResultInterface} result
      * @return {Promise<ImageResultInterface>} result
      */
     async processImage(entity, result) {
+        const sizePromises = this._sizes.getSizes().map((size) => {
+            return new Promise((resolve) => {
+                resolve(true);
+            });
+        });
         return Promise.resolve(result);
     }
 }
